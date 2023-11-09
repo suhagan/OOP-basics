3fc46e79709b0b2aaf524f7365a85da1c165c42a
Suhagan Mostahid <suhaganmostahid@yahoo.com>
2023-11-09 08:44:42 +0100

    now uploading codes to the repository.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_05_The_basics_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get area of a circle with radius 5
+            int radius1 = 5;
+            circleClass circle1 = new circleClass(radius1);
+            double area1 = circle1.getCircleArea();
+            Console.WriteLine("The area: "+area1+ " is of the circle with radius " + radius1);
+            
+            // Get area of a circle with radius 6
+            int radius2 = 6;
+            circleClass circle2 = new circleClass(radius2);
+            double area2 = circle2.getCircleArea();
+            Console.WriteLine("The area: " + area2 + " is of the circle with radius " + radius2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ circleClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab_05_The_basics_OOP
+{
+    class circleClass
+    {
+        float PI = 3.141f;
+        int _Radius;
+
+        public circleClass(int radius)
+        {
+            this._Radius = radius;
+        }
+
+        public double getCircleArea()
+        {
+            return PI * _Radius * _Radius;
+        }
+    }
+}
